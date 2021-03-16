b6ea0b59571fcfe883aaef47e7d676791cb721d9
carolinetan <caroline.kh.tan@gmail.com>
2021-03-16 21:25:19 +0800

    did some challenges for tips

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,18 +1,36 @@
 'use strict';
 
-const calcAverage = (num1, num2, num3) => (num1 + num2 + num3) / 3;
+// Challenge 2
+// const calcAverage = (num1, num2, num3) => (num1 + num2 + num3) / 3;
 
-const dolphinsAverage = calcAverage(44, 23, 71);
-const koalasAverage = calcAverage(65, 54, 49);
+// const dolphinsAverage = calcAverage(44, 23, 71);
+// const koalasAverage = calcAverage(65, 54, 49);
 
-const checkWinner = function (dolphinsAverage, koalasAverage) {
-    if (dolphinsAverage > koalasAverage) {
-        console.log(`Dolphins wins (${dolphinsAverage} vs. ${koalasAverage})`);
-    } else {
-        console.log(`Koalas wins (${koalasAverage} vs. ${dolphinsAverage})`); 
-    };
+// const checkWinner = function (dolphinsAverage, koalasAverage) {
+//     if (dolphinsAverage > koalasAverage) {
+//         console.log(`Dolphins wins (${dolphinsAverage} vs. ${koalasAverage})`);
+//     } else {
+//         console.log(`Koalas wins (${koalasAverage} vs. ${dolphinsAverage})`); 
+//     };
+// }
+
+// checkWinner(dolphinsAverage, koalasAverage);
+
+
+// Challenge 4
+
+const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+const tips = [];
+const totals = [];
+
+const calcTip = function (bill) {
+    let tip = 0;
+    bill >= 50 && bill <=300 ? (tip = 15/100) : (tip = 20/100);
+    let tipAmount = bill * tip;
+    tips.push(tipAmount);
+    let totalAmount = tipAmount;
+    totals.push(totalAmount);
+    return `Tip: ${tipAmount}. Total Amount: ${totalAmount}`;
 }
 
-checkWinner(dolphinsAverage, koalasAverage);
-
-
+console.log(calcTip(100));
