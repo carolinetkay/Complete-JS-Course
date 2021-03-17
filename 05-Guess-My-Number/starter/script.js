290c9f5676b2644f5a75bc9f4ae1f775ba5dbf90
carolinetan <caroline.kh.tan@gmail.com>
2021-03-17 18:11:09 +0800

    added guessing logic checking

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,3 +1,23 @@
 'use strict';
 
-console.log(document.querySelector('.message'));
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+// document.querySelector('.number').textContent = secretNumber;
+console.log(secretNumber);
+
+const eventHandler = function () {
+  const guessValue = Number(document.querySelector('.guess').value);
+  let message = document.querySelector('.message');
+  message.textContent = 'Correct Number! 🙊';
+
+  if (!guessValue) {
+    message.textContent = 'Error! No number 🙈';
+  } else if (guessValue === secretNumber) {
+    message.textContent = 'Congratulations! 🐳';
+  } else if (guessValue > secretNumber) {
+    message.textContent = 'Too high!';
+  } else if (guessValue < secretNumber) {
+    message.textContent = 'Too low!';
+  }
+};
+
+document.querySelector('.check').addEventListener('click', eventHandler);
