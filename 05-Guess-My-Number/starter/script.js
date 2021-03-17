c7507ea8facbd862a7705b6b4543a039a0d202fb
carolinetan <caroline.kh.tan@gmail.com>
2021-03-17 18:31:44 +0800

    adding css style manipulation when player wins

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,22 +1,38 @@
 'use strict';
 
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
-// document.querySelector('.number').textContent = secretNumber;
 console.log(secretNumber);
+
+let score = 20;
+const scoreValue = document.querySelector('.score');
+
+const htmlBody = document.querySelector('body');
+const numberBox = document.querySelector('.number');
 
 const eventHandler = function () {
   const guessValue = Number(document.querySelector('.guess').value);
   let message = document.querySelector('.message');
   message.textContent = 'Correct Number! 🙊';
 
-  if (!guessValue) {
-    message.textContent = 'Error! No number 🙈';
-  } else if (guessValue === secretNumber) {
-    message.textContent = 'Congratulations! 🐳';
-  } else if (guessValue > secretNumber) {
-    message.textContent = 'Too high!';
-  } else if (guessValue < secretNumber) {
-    message.textContent = 'Too low!';
+  if (score > 1) {
+    if (!guessValue) {
+      message.textContent = 'Error! No number 🙈';
+    } else if (guessValue === secretNumber) {
+      message.textContent = 'Congratulations! 🐳';
+      htmlBody.style.backgroundColor = 'green';
+      numberBox.style.width = '30rem';
+      numberBox.textContent = secretNumber;
+    } else if (guessValue > secretNumber) {
+      message.textContent = 'Too high!';
+      score--;
+      scoreValue.textContent = score;
+    } else if (guessValue < secretNumber) {
+      message.textContent = 'Too low!';
+      score--;
+      scoreValue.textContent = score;
+    }
+  } else {
+    message.textContent = 'Game Over! 🌊';
   }
 };
 
