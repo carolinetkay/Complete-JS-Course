3f94b692fbc9bc0fe773cbb2630622071691c2b2
carolinetan <caroline.kh.tan@gmail.com>
2021-03-18 14:53:15 +0800

    showing and closing modals using JS event listeners

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,28 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+// event listener for showing the modal with overlay
+const eventHandler = function () {
+    console.log('button clicked');
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+
+}
+
+btnsOpenModal.forEach((btn) => {
+    btn.addEventListener('click', eventHandler)
+})
+
+// event listener for closing the modal once shown to users
+const closeHandler = function () {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+}
+
+
+btnCloseModal.addEventListener('click', closeHandler);
+
